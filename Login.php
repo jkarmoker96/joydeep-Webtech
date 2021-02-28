010d87a0de7d2968803726c14c737dfadd49dc0e
jkarmoker96 <72846538+jkarmoker96@users.noreply.github.com>
2021-02-28 23:22:48 +0600

    Lab 4 updated
    This is few codes of lab task4.

---
+++ Login.php
@@ -0,0 +1,29 @@
+<div id="containersDiv">
+
+ 		<?php include ('C:\xampp\htdocs\Newtask\C.Login\header1.php');?>
+				
+
+			
+
+
+    <div>
+        <?php include ('1.php');?>
+    </div>
+					
+
+
+
+
+		
+		<?php
+		 include ('C:\xampp\htdocs\Newtask\C.Login\footer.php');
+		?>
+		
+
+</div>
+
+<?php
+
+$_SESSION["fname"] = $name;
+
+?>
